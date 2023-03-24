df10c3f58e74ea1d5d961defb863323c39b23df5
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-03-24 11:24:48 +0000

    get POST request to work and render description of today's horoscope for the selected sign onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import './index.css';
-import DailyHoroscope from './DailyHoroscope';
+// import DailyHoroscope from './DailyHoroscope';
+import React, { useState, useEffect } from 'react';
+//
 
 function App() {
+  const [horoscope, setHoroscope] = useState('');
+  const [sign, setSign] = useState('cancer')
+  
+    useEffect(() => {
+      fetch(`${import.meta.env.VITE_APP_BASE_URL}?sign=${sign}&day=today`, {
+        method: 'POST'
+      })
+        .then((res) => res.json())
+        .then (json => {setHoroscope((json.description))
+        console.log(json)})
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }, [sign]);
+
   return (
     <>
       <nav className="text-3xl font-bold my-5 mx-3">Rising Sign</nav>
@@ -13,82 +30,83 @@ function App() {
           Please choose your zodiac sign.
         </h2>
         <section className="grid gap-4 grid-cols-1 md:grid-cols-3 l:grid-cols-4 xl:grid-cols-6">
-          <a>
+          <button onClick={() => setSign('aries')}>
             <div className="bg-slate-300">
               <h3>Aries</h3>
               <p>Mar 21 - Apr 19</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('taurius')}>
             <div className="bg-slate-300">
               <h3>Taurius</h3>
               <p>Apr 20 - May 20</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('gemini')}>
             <div className="bg-slate-300">
               <h3>Gemini</h3>
               <p>May 21 - Jun 20</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('cancer')}>
             <div className="bg-slate-300">
               <h3>Cancer</h3>
               <p>Jun 21 - Jul 22</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('leo')}>
             <div className="bg-slate-300">
               <h3>Leo</h3>
               <p>Jul 23 - Aug 22</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('virgo')}>
             <div className="bg-slate-300">
               <h3>Virgo</h3>
               <p>Apr 23 - Sep 22</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('libra')}>
             <div className="bg-slate-300">
               <h3>Libra</h3>
               <p>Sep 23 - Oct 22</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('scorpio')}>
             <div className="bg-slate-300">
               <h3>Scorpio</h3>
               <p>Oct 23 - Nov 21</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('sagittarius')}>
             <div className="bg-slate-300">
               <h3>Sagittarius</h3>
               <p>Nov 22 - Dec 21</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('capricorn')}>
             <div className="bg-slate-300">
               <h3>Capricorn</h3>
               <p>Dec 22 - Jan 19</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('aquarius')}>
             <div className="bg-slate-300">
               <h3>Aquarius</h3>
               <p>Jan 20 - Feb 18</p>
             </div>
-          </a>
-          <a>
+          </button>
+          <button onClick={() => setSign('pisces')}>
             <div className="bg-slate-300">
               <h3>Pisces</h3>
               <p>Feb 19 - Mar 20</p>
             </div>
-          </a>
+          </button>
         </section>
       </div>
       <div className="App">
-        <DailyHoroscope />
+        {horoscope}
+        {/* <DailyHoroscope /> */}
       </div>
     </>
   );
--- src/DailyHoroscope.tsx
+++ src/DailyHoroscope.tsx
@@ -1,28 +1,41 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
 
-function DailyHoroscope() {
+// function DailyHoroscope() {
 
-  const [advice, setAdvice] = useState('')
+//   const [horoscope, setHoroscope] = useState('')
 
-//refactor into POST request
+// //refactor into POST request
 
-  const url = "https://aztro.sameerkumar.website?sign=aries&day=today"
-  const fetchData = async () => {
-    try {
-      const response = await fetch(url);
-      const json = await response.json()
-      setAdvice(json.slip.advice)
-    } catch (error) {
-      console.log("error", error)
-    }
-  }
-  useEffect(() => {
-    fetchData()
-  }, [])
+//   // const url = "https://aztro.sameerkumar.website?sign=aries&day=today"
+//   // const fetchData = async () => {
+//   //   try {
+//   //     const response = await fetch(url);
+//   //     const json = await response.json()
+//   //     console.log(json)
+//   //     setHoroscope(json.description)
+//   //   } catch (error) {
+//   //     console.log("error", error)
+//   //   }
+//   // }
+//   useEffect(() => {
+//     fetch('https://aztro.sameerkumar.website?sign=aries&day=today', {
+//       method: "POST",
+//       headers: {"Content-Type": "application/json"},
+//       body: JSON.stringify()
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data)
+//         setHoroscope(data)
+//       })
+//       .catch((err) => {
+//         console.log(err.message)
+//       })
+//   }, [])
 
-  return (
-    <div>{advice}</div>
-  )
-}
+//   return (
+//     <div>{horoscope}</div>
+//   )
+// }
 
-export default DailyHoroscope
+// export default DailyHoroscope
