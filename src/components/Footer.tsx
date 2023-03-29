d7430fe9e34cafb272aa7dd5a159539ad4295ea0
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-03-29 17:23:20 +0100

    add layout with navbar and footer to about, explore and horoscope pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import SubscribeForm from './SubscribeForm';
 
 function Footer() {
@@ -5,11 +6,18 @@ function Footer() {
     <footer className=" mt-auto flex flex-col items-center h-72 justify-center bottom-0 w-[100%] bg-[#fffafd] text-center border-solid border-t-[2px] border-[#fcf3f8]">
       <div className="mt-20 grid grid-cols-1 gap-x-2 md:grid-cols-2 lg:gap-x-60 ">
         <div className="flex flex-col invisible gap-y-3 text-left md:visible">
-          <h3>Horoscopes</h3>
+          <Link to="/horoscopes" className="hover:text-indigo-600">
+            {' '}
+            <h3>Horoscopes</h3>
+          </Link>
           <h3>Compatibility</h3>
           <h3>Lucky Numbers</h3>
-          <h3>Explore</h3>
-          <h3>About</h3>
+          <Link to="/explore" className="hover:text-indigo-600">
+            <h3>Explore</h3>
+          </Link>
+          <Link to="/about" className="hover:text-indigo-600">
+            <h3>About</h3>
+          </Link>
         </div>
         <SubscribeForm />
       </div>
--- src/pages/Horoscopes.tsx
+++ src/pages/Horoscopes.tsx
@@ -1,12 +1,17 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Horoscopes() {
   return (
-   ( <div>
+    <div>
       <Header />
+      <div className="my-20 flex flex-col justify-center items-center gap-y-8 gap-x-20 md:flex-row tracking-wider">
+        <h1 className="duration-200 hover:text-indigo-600">YESTERDAY</h1>
+        <h1 className=" p-4 border-b-4 border-solid border-slate-600 duration-200 hover:text-indigo-600">TODAY</h1>
+        <h1 className="duration-200 hover:text-indigo-600">TOMORROW</h1>
+      </div>
       <Footer />
-    </div>)
+    </div>
   );
 }
 export default Horoscopes;
