693fb6e8d34b66dccfc4ac789c2ee4fde9337400
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-04-12 16:26:26 +0100

    install progress bar dependancy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import './index.css';
 import Home from './Home';
 import {
   Route,
+  Router,
   Routes,
 } from 'react-router-dom';
 import About from './pages/About';
 import NoMatch from './pages/NoMatch';
 import Explore from './pages/Explore';
 import Horoscopes from './pages/Horoscopes';
+import ProgressBar from '@badrap/bar-of-progress';
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+});
 
 
 export function App() {
