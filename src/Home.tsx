491b849adb3abd06dd211449ca3f1004178aa654
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-05-11 18:01:38 +0100

    Refactor backup data implementation in case the API returns an error

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import './index.css';
-import AdsComponent from './components/AdsComponent';
 import Logo from './assets/backgrounds/logo.png';
 import { useState, useEffect } from 'react';
 import backupData from './data/backUpData';
@@ -10,20 +9,21 @@ import Signs from './components/Signs';
 import Horoscope from './components/Horoscope';
 import Galaxy from './assets/backgrounds/cosmos2.jpg';
 
+interface HoroscopeData {
+  description: string;
+  compatibility: string;
+  mood: string;
+  color: string;
+  lucky_number: string;
+  lucky_time: string;
+}
+
+interface HoroscopeState {
+  [key: string]: HoroscopeData;
+}
+
 function Home() {
-  // const [aries, setAries] = useState<string | null>('');
-  // const [taurus, setTaurus] = useState<string | null>('');
-  // const [gemini, setGemini] = useState<string | null>('');
-  // const [cancer, setCancer] = useState<string | null>('');
-  // const [leo, setLeo] = useState<string | null>('');
-  // const [virgo, setVirgo] = useState<string | null>('');
-  // const [libra, setLibra] = useState<string | null>('');
-  // const [scorpio, setScorpio] = useState<string | null>('');
-  // const [sagittarius, setSagittarius] = useState<string | null>('');
-  // const [capricorn, setCapricorn] = useState<string | null>('');
-  // const [aquarius, setAquarius] = useState<string | null>('');
-  // const [pisces, setPisces] = useState<string | null>('');
-   const [horoscopes, setHoroscopes] = useState({});
+   const [horoscopes, setHoroscopes] = useState<HoroscopeState>({});
 
   //Use a helper function to get the current date
   const date = getToday();
@@ -73,287 +73,19 @@ function Home() {
       fetchData();
     }, []);
 
-    const getHoroscopeData = async (horoscope) => {
+    const getHoroscopeData = async (horoscope : string) => {
+      try {
       const url = `${
       import.meta.env.VITE_APP_BASE_URL}&sign=${horoscope}&date=${date}`;
        const response = await fetch(url, fetchOptions);
        const data = await response.json();
-       return data;
+       return data; }
+       catch (error) {
+         console.error(error);
+          const backup = backupData.find((sign) => sign.name === horoscope);
+          return backup;
+       }
     };
-
-
-  // //Fetch Aries
-  // const getAries = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=aries&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     //Save the data to local storage
-  //     localStorage.setItem('aries', data.description);
-  //     //Save the date timestamp to local storage
-  //     localStorage.setItem('ariesDate', date);
-  //     setAries(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Taurus
-  // const getTaurus = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=taurus&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('taurus', data.description);
-  //     localStorage.setItem('taurusDate', date);
-  //     setTaurus(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Gemini
-  // const getGemini = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=gemini&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('gemini', data.description);
-  //     localStorage.setItem('geminiDate', date);
-  //     setGemini(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Cancer
-  // const getCancer = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=cancer&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('cancer', data.description);
-  //     localStorage.setItem('cancerDate', date);
-  //     setCancer(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Leo
-  // const getLeo = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=leo&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('leo', data.description);
-  //     localStorage.setItem('leoDate', date);
-  //     setLeo(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Virgo
-  // const getVirgo = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=virgo&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('virgo', data.description);
-  //     localStorage.setItem('virgoDate', date);
-  //     setVirgo(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Libra
-  // const getLibra = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=libra&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('libra', data.description);
-  //     localStorage.setItem('libraDate', date);
-  //     setLibra(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Scorpio
-  // const getScorpio = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=scorpio&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('scorpio', data.description);
-  //     localStorage.setItem('scorpioDate', date);
-  //     setScorpio(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Sagittarius
-  // const getSagittarius = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=sagittarius&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('sagittarius', data.description);
-  //     localStorage.setItem('sagittariusDate', date);
-  //     setSagittarius(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Capricorn
-  // const getCapricorn = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=capricorn&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('capricorn', data.description);
-  //     localStorage.setItem('capricornDate', date);
-  //     setCapricorn(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Aquarius
-  // const getAquarius = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=aquarius&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('aquarius', data.description);
-  //     localStorage.setItem('aqauriusDate', date);
-  //     setAquarius(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //Fetch Pisces
-  // const getPisces = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_APP_BASE_URL}&sign=pisces&date=${date}`,
-  //       fetchOptions
-  //     );
-  //     const data = await response.json();
-  //     localStorage.setItem('pisces', data.description);
-  //     localStorage.setItem('piscesDate', date);
-  //     setPisces(data.description);
-  //     console.log(data.description);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   //Check if the timestamp for each zodiac sign is the same as the current date
-
-  //   ///////////////////////Fetch Aries////////////////////////
-  //   if (window.localStorage.getItem('ariesDate') !== date) {
-  //     getAries();
-  //   } else {
-  //     setAries(window.localStorage.getItem('aries'));
-  //   }
-
-  //   ///////////////////////Fetch Taurus////////////////////////
-  //   if (window.localStorage.getItem('taurusDate') !== date) {
-  //     getTaurus();
-  //   } else {
-  //     setTaurus(window.localStorage.getItem('taurus'));
-  //   }
-  //   ///////////////////////Fetch Gemini////////////////////////
-  //   if (window.localStorage.getItem('geminiDate') !== date) {
-  //     getGemini();
-  //   } else {
-  //     setGemini(window.localStorage.getItem('gemini'));
-  //   }
-  //   ///////////////////////Fetch Cancer////////////////////////
-  //   if (window.localStorage.getItem('cancerDate') !== date) {
-  //     getCancer();
-  //   } else {
-  //     setCancer(window.localStorage.getItem('cancer'));
-  //   }
-  //   ///////////////////////Fetch Leo////////////////////////
-  //   if (window.localStorage.getItem('leoDate') !== date) {
-  //     getLeo();
-  //   } else {
-  //     setLeo(window.localStorage.getItem('leo'));
-  //   }
-  //   ///////////////////////Fetch Virgo////////////////////////
-  //   if (window.localStorage.getItem('virgoDate') !== date) {
-  //     getVirgo();
-  //   } else {
-  //     setVirgo(window.localStorage.getItem('virgo'));
-  //   }
-  //   ///////////////////////Fetch Libra////////////////////////
-  //   if (window.localStorage.getItem('libraDate') !== date) {
-  //     getLibra();
-  //   } else {
-  //     setLibra(window.localStorage.getItem('libra'));
-  //   }
-  //   ///////////////////////Fetch Scorpio////////////////////////
-  //   if (window.localStorage.getItem('scorpioDate') !== date) {
-  //     getScorpio();
-  //   } else {
-  //     setScorpio(window.localStorage.getItem('scorpio'));
-  //   }
-  //   ///////////////////////Fetch Sagittarius////////////////////////
-  //   if (window.localStorage.getItem('sagittariusDate') !== date) {
-  //     getSagittarius();
-  //   } else {
-  //     setSagittarius(window.localStorage.getItem('sagittarius'));
-  //   }
-  //   ///////////////////////Fetch Capricorn////////////////////////
-  //   if (window.localStorage.getItem('capricornDate') !== date) {
-  //     getCapricorn();
-  //   } else {
-  //     setCapricorn(window.localStorage.getItem('capricorn'));
-  //   }
-  //   ///////////////////////Fetch Aquarius////////////////////////
-  //   if (window.localStorage.getItem('aquariusDate') !== date) {
-  //     getAquarius();
-  //   } else {
-  //     setAquarius(window.localStorage.getItem('aquarius'));
-  //   }
-  //   ///////////////////////Fetch Pisces////////////////////////
-  //   if (window.localStorage.getItem('piscesDate') !== date) {
-  //     getPisces();
-  //   } else {
-  //     setPisces(window.localStorage.getItem('pisces'));
-  //   }
-  // }, []);
 
   return (
     <>
@@ -379,48 +111,10 @@ function Home() {
       </section>
       <div className="mx-5 md:mx-40 my-6 grid grid-cols-1 gap-x-20 gap-y-5 lg:grid-cols-2 sm:grid-cols-1">
         {/*If API responds with error, return backup horoscope data */}
-        <div>
           {Object.entries(horoscopes).map(([key, value]) => (
-            
-           <Horoscope children={value.description} name={key} key={key}/>
+           <Horoscope children={value.description} name={key.charAt(0).toUpperCase() + key.substring(1)} key={key}/>
           ))}
-        </div>
-        {/* <Horoscope children={aries || backupData[0].description} name="Aries" />
-        <Horoscope
-          children={taurus || backupData[1].description}
-          name="Taurus"
-        />
-        <Horoscope
-          children={gemini || backupData[2].description}
-          name="Gemini"
-        />
-        <Horoscope
-          children={cancer || backupData[3].description}
-          name="Cancer"
-        />
-        <Horoscope children={leo || backupData[4].description} name="Leo" />
-        <Horoscope children={virgo || backupData[5].description} name="Virgo" />
-        <Horoscope children={libra || backupData[6].description} name="Libra" />
-        <Horoscope
-          children={scorpio || backupData[7].description}
-          name="Scoprio"
-        />
-        <Horoscope
-          children={sagittarius || backupData[8].description}
-          name="Sagittarius"
-        />
-        <Horoscope
-          children={capricorn || backupData[9].description}
-          name="Capricorn"
-        />
-        <Horoscope
-          children={aquarius || backupData[10].description}
-          name="Aquarius"
-        />
-        <Horoscope
-          children={pisces || backupData[11].description}
-          name="Pisces"
-        /> */}
+
       </div>
       <section className="mx-5 md:mx-40 my-5">
         <h1 className="text-lg py-3 mb-3 md:text-2xl font-bold tracking-wide">
--- src/data/backUpData.tsx
+++ src/data/backUpData.tsx
@@ -3,73 +3,73 @@
 const backupData = [
   {
     id: 1,
-    name: 'Aries',
+    name: 'aries',
     description:
       "You may feel a strong urge to take action today, Aries. Trust your instincts, but also take a moment to consider the consequences of your actions. Your impulsiveness could lead you into trouble if you're not careful.",
   },
   {
     id: 2,
-    name: 'Taurus',
+    name: 'taurus',
     description:
       'You may be feeling more emotional than usual today, Taurus. This could be a good time to connect with loved ones and let them know how much you care. Remember to take care of yourself as well.',
   },
   {
     id: 3,
-    name: 'Gemini',
+    name: 'gemini',
     description:
       'Your communication skills are especially strong today, Gemini. You may find it easier than usual to connect with others and get your point across. Use this to your advantage in both your personal and professional life.',
   },
   {
     id: 4,
-    name: 'Cancer',
+    name: 'cancer',
     description:
       "You may be feeling a bit moody today, Cancer. This is a good time to focus on self-care and nurturing yourself. Don't be afraid to reach out to loved ones for support if you need it.",
   },
   {
     id: 5,
-    name: 'Leo',
+    name: 'leo',
     description:
       'You may feel a strong urge to take charge today, Leo. This could be a good time to step up and show your leadership skills. Just be sure to listen to others and take their opinions into account as well.',
   },
   {
     id: 6,
-    name: 'Virgo',
+    name: 'virgo',
     description:
       'You may be feeling more analytical than usual today, Virgo. This could be a good time to focus on tasks that require attention to detail. Just be sure to take breaks and give your mind a rest when needed.',
   },
   {
     id: 7,
-    name: 'Libra',
+    name: 'libra',
     description:
       "You may be feeling more creative than usual today, Libra. This could be a good time to explore your artistic side and express yourself in new ways. Don't be afraid to try something new.",
   },
   {
     id: 8,
-    name: 'Scorpio',
+    name: 'scorpio',
     description:
       'You may be feeling more intense than usual today, Scorpio. This could be a good time to focus on your passions and pursue what makes you feel alive. Just be sure to channel your energy in a positive way.',
   },
   {
     id: 9,
-    name: 'Sagittarius',
+    name: 'sagittarius',
     description:
       'You may be feeling more adventurous than usual today, Sagittarius. This could be a good time to explore new places and try new things. Just be sure to stay safe and take precautions when necessary.',
   },
   {
     id: 10,
-    name: 'Capricorn',
+    name: 'capricorn',
     description:
       ' You may be feeling more practical than usual today, Capricorn. This could be a good time to focus on your goals and make progress towards achieving them. Just remember to take breaks and enjoy the journey as well.',
   },
   {
     id: 11,
-    name: 'Aquarius',
+    name: 'aquarius',
     description:
       'You may be feeling more independent than usual today, Aquarius. This could be a good time to pursue your own interests and focus on your personal growth. Just be sure to stay connected with loved ones as well.',
   },
   {
     id: 12,
-    name: 'Pisces',
+    name: 'pisces',
     description:
       'You may be feeling more intuitive than usual today, Pisces. This could be a good time to trust your instincts and listen to your inner voice. Just be sure to take practical steps towards achieving your goals as well.',
   },
